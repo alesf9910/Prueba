55a31f11fed4c6e4c968308e04cfd23d849cdda5
Alejandro <acronus2008@gmail.com>
2020-03-12 10:31:18 -0700

    implement fake post result

---
+++ src/main/java/com/fyself/post/service/post/impl/PostServiceImpl.java
@@ -0,0 +1,35 @@
+package com.fyself.post.service.post.impl;
+
+import com.fyself.post.service.post.PostService;
+import com.fyself.post.service.post.contract.to.PostTO;
+import com.fyself.seedwork.service.context.FySelfContext;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Service("postService")
+@Validated
+public class PostServiceImpl implements PostService {
+    @Override
+    public Mono<String> create(@NotNull @Valid PostTO to, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> update(@NotNull @Valid PostTO to, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<PostTO> load(@NotNull String id, FySelfContext context) {
+        return Mono.just(new PostTO().withOwner("5e5e9440e3e14e1c49d8ebbf"));
+    }
+
+    @Override
+    public Mono<Void> delete(@NotNull String id, FySelfContext context) {
+        return null;
+    }
+}
