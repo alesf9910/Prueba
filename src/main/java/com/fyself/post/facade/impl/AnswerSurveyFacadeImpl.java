78f98f927cd36198d3769a814810cfbaf78d3ba0
Alejandro <acronus2008@gmail.com>
2020-03-13 12:03:35 -0700

    implement facade and create method

---
+++ src/main/java/com/fyself/post/facade/impl/AnswerSurveyFacadeImpl.java
@@ -0,0 +1,57 @@
+package com.fyself.post.facade.impl;
+
+import com.fyself.post.facade.AnswerSurveyFacade;
+import com.fyself.post.service.post.AnswerSurveyService;
+import com.fyself.post.service.post.contract.to.AnswerSurveyTO;
+import com.fyself.post.service.post.contract.to.PostReportTO;
+import com.fyself.post.service.post.contract.to.criteria.AnswerSurveyCriteriaTO;
+import com.fyself.seedwork.facade.Result;
+import com.fyself.seedwork.facade.stereotype.Facade;
+import com.fyself.seedwork.service.PagedList;
+import com.fyself.seedwork.service.context.FySelfContext;
+import reactor.core.publisher.Mono;
+
+@Facade("answerSurveyFacade")
+public class AnswerSurveyFacadeImpl implements AnswerSurveyFacade {
+
+    final AnswerSurveyService service;
+
+    public AnswerSurveyFacadeImpl(AnswerSurveyService service) {
+        this.service = service;
+    }
+
+    @Override
+    public Mono<Result<String>> create(AnswerSurveyTO to, FySelfContext exchange) {
+        return service.add(to, exchange).map(Result::successful);
+    }
+
+    @Override
+    public Mono<Result<Void>> update(AnswerSurveyTO template, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<Result<Void>> delete(String id, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<Result<PostReportTO>> load(String id, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<Result<PagedList<PostReportTO>>> search(AnswerSurveyCriteriaTO criteria, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<Result<PagedList<PostReportTO>>> searchByMe(AnswerSurveyCriteriaTO criteria, FySelfContext context) {
+        return null;
+    }
+
+    @Override
+    public Mono<Result<PagedList<PostReportTO>>> searchToMe(AnswerSurveyCriteriaTO criteria, FySelfContext context) {
+        return null;
+    }
+}
