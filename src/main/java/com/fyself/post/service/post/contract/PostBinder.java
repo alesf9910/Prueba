78c89c1bd90596dbedda8404acbf673c4d17e8e5
Josue <jmmarin9003@gmail.com>
2020-03-13 11:36:10 -0400

    Added support to LinkContent, ImageContent and TextContent in CRUD Post

--- src/main/java/com/fyself/post/service/post/contract/PostBinder.java
+++ src/main/java/com/fyself/post/service/post/contract/PostBinder.java
@@ -1,11 +1,11 @@
 package com.fyself.post.service.post.contract;
 
-import com.fyself.post.service.post.contract.to.ContentTO;
-import com.fyself.post.service.post.contract.to.LinkContentTO;
-import com.fyself.post.service.post.contract.to.PostTO;
+import com.fyself.post.service.post.contract.to.*;
 import com.fyself.post.service.post.datasource.domain.Post;
 import com.fyself.post.service.post.datasource.domain.subentities.Content;
+import com.fyself.post.service.post.datasource.domain.subentities.ImageContent;
 import com.fyself.post.service.post.datasource.domain.subentities.LinkContent;
+import com.fyself.post.service.post.datasource.domain.subentities.TextContent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -24,27 +24,36 @@ public interface PostBinder {
     @Mapping(target = "content", expression = "java(bind(source.getContent()))")
     Post bind(PostTO source);
 
+    @Mapping(target = "content", expression = "java(bind(source.getContent()))")
+    PostTO bind(Post source);
+
     default Content bind(ContentTO source) {
         if (source instanceof LinkContentTO)
             return this.bind((LinkContentTO) source);
+        if (source instanceof ImageContentTO)
+            return this.bind((ImageContentTO) source);
+        if (source instanceof TextContentTO)
+            return this.bind((TextContentTO) source);
         return null;
     }
 
     LinkContent bind(LinkContentTO source);
-
-    default PostTO bind(Post source) {
-        PostTO post = new PostTO();
-        post.setContent(this.bind(source.getContent()));
-        return post;
-    }
+    ImageContent bind(ImageContentTO source);
+    TextContent bind(TextContentTO source);
 
     default ContentTO bind(Content source) {
         if (source instanceof LinkContent)
             return this.bind((LinkContent) source);
+        if (source instanceof ImageContent)
+            return this.bind((ImageContent) source);
+        if (source instanceof TextContent)
+            return this.bind((TextContent) source);
         return null;
     }
 
     LinkContentTO bind(LinkContent source);
+    ImageContentTO bind(ImageContent source);
+    TextContentTO bind(TextContent source);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "owner", ignore = true)
