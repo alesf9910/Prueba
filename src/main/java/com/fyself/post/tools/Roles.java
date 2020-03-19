5d3df010b2a19b17b77837aa800a3da23bbb6ffe
Alejandro <acronus2008@gmail.com>
2020-03-19 10:29:21 -0700

    adding rol admin for comments

--- src/main/java/com/fyself/post/tools/Roles.java
+++ src/main/java/com/fyself/post/tools/Roles.java
@@ -3,4 +3,5 @@ package com.fyself.post.tools;
 public final class Roles {
     static public String CATEGORY_ADMIN = "USER:READ";
     static public String TAXONOMY_ADMIN = "USER:READ";
+    static public String POST_ADMIN = "POST:ADMIN";
 }
