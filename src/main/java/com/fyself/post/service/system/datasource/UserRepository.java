1d30b9b7d9347ab02ab5331e66e0747c5b9de5ff
Alejandro <acronus2008@gmail.com>
2020-04-14 19:53:12 -0700

    improve web client

--- src/main/java/com/fyself/post/service/system/datasource/UserRepository.java
+++ src/main/java/com/fyself/post/service/system/datasource/UserRepository.java
@@ -1,17 +1,20 @@
 package com.fyself.post.service.system.datasource;
 
+import com.fyself.post.service.system.contract.to.ContactTO;
 import com.fyself.post.service.system.contract.to.UserTO;
 import com.fyself.seedwork.service.context.FySelfContext;
 import reactor.core.publisher.Mono;
 
 /**
- *
- *
  * @author jmmarin
- * @since 0.0.2
+ * @author Alejandro
+ * @since 0.0.3
  */
-public interface UserRepository  {
+public interface UserRepository {
 
     Mono<Boolean> existUser(String userId, FySelfContext context);
+
     Mono<UserTO> getUser(String userId, FySelfContext context);
+
+    Mono<ContactTO> getContact(String userId, FySelfContext context);
 }
