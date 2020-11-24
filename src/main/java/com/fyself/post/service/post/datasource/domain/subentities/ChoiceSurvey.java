94a069e9dbc51abace86f94d2b95748b6f242232
unknown <ajborgeslag@gmail.com>
2020-11-24 12:18:57 -0500

    Feat: Push egain changed on Post Survei options order.

--- src/main/java/com/fyself/post/service/post/datasource/domain/subentities/ChoiceSurvey.java
+++ src/main/java/com/fyself/post/service/post/datasource/domain/subentities/ChoiceSurvey.java
@@ -1,18 +1,29 @@
 package com.fyself.post.service.post.datasource.domain.subentities;
 
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class ChoiceSurvey extends SurveyContent {
 
-    private Set<Map<String, Object>> choices;
+    /*private Set<Map<String, Object>> choices;*/
 
-    public Set<Map<String, Object>> getChoices() {
+    private List<Map<String, Object>> choices;
+
+    /*public Set<Map<String, Object>> getChoices() {
         return choices;
     }
 
     public void setChoices(Set<Map<String, Object>> choices) {
+        this.choices = choices;
+    }*/
+
+    public List<Map<String, Object>> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Map<String, Object>> choices) {
         this.choices = choices;
     }
 }
