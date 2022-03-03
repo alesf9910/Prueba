352585b9b8822322904a7119acf599796fd871c0
Rai <raidanrivero@gmail.com>
2022-03-03 13:57:15 -0500

    Feat: Fixing order in hierarchy survey (updated)

--- src/main/java/com/fyself/post/service/post/contract/to/HierarchySurveyTO.java
+++ src/main/java/com/fyself/post/service/post/contract/to/HierarchySurveyTO.java
@@ -2,29 +2,30 @@ package com.fyself.post.service.post.contract.to;
 
 
 import javax.validation.constraints.NotEmpty;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HierarchySurveyTO extends SurveyContentTO {
     private static final long serialVersionUID = 5688252080662594286L;
-    private Set<Map<String, Object>> options;
+    private List<Map<String, Object>> options;
 
     @NotEmpty
-    public Set<Map<String, Object>> getOptions() {
+    public List<Map<String, Object>> getOptions() {
         return options;
     }
 
-    public void setOptions(Set<Map<String, Object>> options) {
+    public void setOptions(List<Map<String, Object>> options) {
         this.options = options;
     }
 
     public HierarchySurveyTO generateOptionsIds() {
-        this.setOptions(this.options.stream()
-                .peek(option ->
-                        option.put("id", UUID.randomUUID().toString()))
-                .collect(Collectors.toSet()));
+        List<Map<String, Object>> result = this.options.stream()
+                .map(option -> {
+                    option.put("id", UUID.randomUUID().toString());
+                    return option;
+                })
+                .collect(Collectors.toList());
+        this.setOptions(result);
         return this;
     }
 }
--- src/main/java/com/fyself/post/service/post/datasource/domain/subentities/HierarchySurvey.java
+++ src/main/java/com/fyself/post/service/post/datasource/domain/subentities/HierarchySurvey.java
@@ -1,18 +1,19 @@
 package com.fyself.post.service.post.datasource.domain.subentities;
 
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class HierarchySurvey extends SurveyContent {
 
-    private Set<Map<String, Object>> options;
+    private List<Map<String, Object>> options;
 
-    public Set<Map<String, Object>> getOptions() {
+    public List<Map<String, Object>> getOptions() {
         return options;
     }
 
-    public void setOptions(Set<Map<String, Object>> options) {
+    public void setOptions(List<Map<String, Object>> options) {
         this.options = options;
     }
 }
