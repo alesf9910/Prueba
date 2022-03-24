e2563900ae6215124c49436cf185eabd536bc231
Rai <raidanrivero@gmail.com>
2022-03-24 14:25:21 -0400

    Feat: Change limit to buffer memory

---
+++ src/main/java/com/fyself/post/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package com.fyself.post.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize( 50 * 1024 * 1024);
+    }
+}
